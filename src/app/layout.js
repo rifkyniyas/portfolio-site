c8fefe442125eb9bfe6a7434b69c2427ebf88080
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2023-07-11 23:10:50 +0530

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import Script from "next/script";
 const poppins = Poppins({ subsets: ["latin"], weight: "400" });
 
 export const metadata = {
@@ -11,6 +12,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <body
         className={`${poppins.className} bg-dark  text-white h-full min-h-screen `}
       >
